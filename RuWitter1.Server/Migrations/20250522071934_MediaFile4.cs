90d3b6cfdb225362f14f2cb74277ca12f898472d
Zhilin Pavel <122386179+ZhilWeb@users.noreply.github.com>
2025-05-29 21:25:18 +0500

    Default Auth1.

---
+++ RuWitter1.Server/Migrations/20250522071934_MediaFile4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RuWitter1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MediaFile4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "MediaFiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "MediaFiles");
+        }
+    }
+}
